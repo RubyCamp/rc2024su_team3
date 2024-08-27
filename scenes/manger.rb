e8893ece1863e50e3d2395ec9346178385df7f72
kirura-nb <kikurage.taka@icloud.com>
2024-08-27 14:09:06 +0900

    シーンの切り替え（オープニングまでの）処理を実装しました。

---
+++ scenes/manger.rb
@@ -0,0 +1,43 @@
+require 'singleton'
+
+module Scenes
+    # シーン（場面）制御用クラス
+    class Manager
+        include Singleton
+
+        def initialize
+            @scenes = {}
+            @current = nil
+        end
+
+        def add(label, director)
+            raise "ディレクターオブジェクト（DirectorBaseを継承したクラスのインスタンス）のみ追加可能です" unless director.is_a?(DirectorBase)
+            @scenes[label.to_sym] = director
+        end
+
+        # カレントシーンの切り替え
+        def set(label)
+            @current = label.to_sym
+        end
+
+        # 1フレームの場面処理の呼び出し
+        def update(opt = {})
+            validate_current
+            @scenes[@current].update(opt)
+        end
+
+        # 1フレームの場面描画処理の呼び出し
+        def draw
+            validate_current
+            @scenes[@current].draw
+        end
+
+        private
+
+        # カレントシーンのラベルとそれに対応するディレクターオブジェクトが実在するかどうかの判定
+        def validate_current
+            raise "setメソッドを実行してカレントシーンを設定してください" unless @current
+            raise "'#{@current}'というラベルを持つシーンは登録されていません" unless @scenes.has_key?(@current)
+        end
+    end
+end
